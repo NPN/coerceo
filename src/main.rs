d32f7e5704edbe28ba2b45004fc409dbd60380d6
Ryan Huang <NPN@users.noreply.github.com>
2017-07-06 22:55:17 -0700

    Add SDL2 window initialization

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+extern crate sdl2;
+
+use sdl2::pixels::Color;
+
+const SCREEN_WIDTH: u32 = 800;
+const SCREEN_HEIGHT: u32 = 800;
+
 fn main() {
-    println!("Hello, world!");
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+    let window = video_subsystem
+        .window("Coerceo", SCREEN_WIDTH, SCREEN_HEIGHT)
+        .position_centered()
+        .build()
+        .unwrap();
+    let mut canvas = window.into_canvas().build().unwrap();
+
+    canvas.set_draw_color(Color::RGB(255, 255, 255));
+    canvas.clear();
+    canvas.present();
+
+    std::thread::sleep(std::time::Duration::from_secs(3));
 }
