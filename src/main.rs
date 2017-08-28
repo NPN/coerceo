e462fa2172198e8cb63503026182f8771e354065
Ryan Huang <NPN@users.noreply.github.com>
2017-08-27 17:55:29 -0700

    Add dear imgui, remove SDL

--- src/main.rs
+++ src/main.rs
@@ -15,28 +15,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-extern crate sdl2;
-
 mod model;
 
-use sdl2::pixels::Color;
-
-const SCREEN_WIDTH: u32 = 800;
-const SCREEN_HEIGHT: u32 = 800;
-
 fn main() {
-    let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
-    let window = video_subsystem
-        .window("Coerceo", SCREEN_WIDTH, SCREEN_HEIGHT)
-        .position_centered()
-        .build()
-        .unwrap();
-    let mut canvas = window.into_canvas().build().unwrap();
-
-    canvas.set_draw_color(Color::RGB(255, 255, 255));
-    canvas.clear();
-    canvas.present();
-
-    std::thread::sleep(std::time::Duration::from_secs(3));
 }
