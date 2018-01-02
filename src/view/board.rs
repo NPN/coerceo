883a0526f7247cf1d6f4fa4bedce4a56df577b14
Ryan Huang <NPN@users.noreply.github.com>
2018-01-01 21:11:05 -0800

    Update copyright years

--- src/view/board.rs
+++ src/view/board.rs
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 Ryan Huang
+ * Copyright (C) 2017-2018 Ryan Huang
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published
