efe56da963f1323a81ecc15f8defb1a9179b8188
Ryan Huang <NPN@users.noreply.github.com>
2018-06-20 19:05:28 -0700

    Only draw UI on mouse event, remove unused code
    We now use run_forever instead of poll_events to only redraw the UI when
    something happens (this works because this game has no animations). For cursor
    movements, we cap the framerate at about 25fps to save CPU time. For mouse
    clicks we draw the UI twice to immediately show the results of the click. We
    also draw one frame before the event loop begins so that the screen isn't empty.
    
    Also, all the keyboard code has been removed, since it's unlikely this game will
    ever support the keyboard or require a text entry.

--- src/view/sys.rs
+++ src/view/sys.rs
@@ -16,11 +16,14 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+use std::time::{Duration, Instant};
+
 use glium::glutin;
 use glium::{Display, Surface};
 use imgui::{ImGui, Ui};
 use imgui_glium_renderer::Renderer;
-use std::time::Instant;
+
+const FRAME_DURATION: Duration = Duration::from_millis(40);
 
 #[derive(Copy, Clone, PartialEq, Debug, Default)]
 struct MouseState {
@@ -47,142 +50,130 @@ pub fn run<F: FnMut(&Ui, (f32, f32)) -> bool>(
 
     imgui.set_ini_filename(None);
 
-    configure_keys(&mut imgui);
-
     let mut last_frame = Instant::now();
     let mut mouse_state = MouseState::default();
-    let mut quit = false;
 
-    loop {
-        events_loop.poll_events(|event| {
-            use glium::glutin::ElementState::Pressed;
-            use glium::glutin::WindowEvent::*;
-            use glium::glutin::{Event, MouseButton, MouseScrollDelta, TouchPhase};
+    // Render one frame before the event loop so the screen isn't empty
+    render(
+        &mut imgui,
+        &mut run_ui,
+        &display,
+        &mut renderer,
+        clear_color,
+        0.1,
+    );
 
-            if let Event::WindowEvent { event, .. } = event {
-                match event {
-                    Closed => quit = true,
-                    KeyboardInput { input, .. } => {
-                        use glium::glutin::VirtualKeyCode as Key;
+    events_loop.run_forever(|event| {
+        use glium::glutin::ElementState::Pressed;
+        use glium::glutin::WindowEvent::*;
+        use glium::glutin::{ControlFlow, Event, MouseButton, VirtualKeyCode};
 
-                        let pressed = input.state == Pressed;
-                        match input.virtual_keycode {
-                            Some(Key::Tab) => imgui.set_key(0, pressed),
-                            Some(Key::Left) => imgui.set_key(1, pressed),
-                            Some(Key::Right) => imgui.set_key(2, pressed),
-                            Some(Key::Up) => imgui.set_key(3, pressed),
-                            Some(Key::Down) => imgui.set_key(4, pressed),
-                            Some(Key::PageUp) => imgui.set_key(5, pressed),
-                            Some(Key::PageDown) => imgui.set_key(6, pressed),
-                            Some(Key::Home) => imgui.set_key(7, pressed),
-                            Some(Key::End) => imgui.set_key(8, pressed),
-                            Some(Key::Delete) => imgui.set_key(9, pressed),
-                            Some(Key::Back) => imgui.set_key(10, pressed),
-                            Some(Key::Return) => imgui.set_key(11, pressed),
-                            Some(Key::Escape) => imgui.set_key(12, pressed),
-                            Some(Key::A) => imgui.set_key(13, pressed),
-                            Some(Key::C) => imgui.set_key(14, pressed),
-                            Some(Key::V) => imgui.set_key(15, pressed),
-                            Some(Key::X) => imgui.set_key(16, pressed),
-                            Some(Key::Y) => imgui.set_key(17, pressed),
-                            Some(Key::Z) => imgui.set_key(18, pressed),
-                            Some(Key::LControl) | Some(Key::RControl) => {
-                                imgui.set_key_ctrl(pressed)
-                            }
-                            Some(Key::LShift) | Some(Key::RShift) => imgui.set_key_shift(pressed),
-                            Some(Key::LAlt) | Some(Key::RAlt) => imgui.set_key_alt(pressed),
-                            Some(Key::LWin) | Some(Key::RWin) => imgui.set_key_super(pressed),
-                            Some(Key::Q) => {
-                                if cfg!(target_os = "macos") && input.modifiers.logo {
-                                    quit = true;
-                                }
-                            }
-                            _ => {}
+        if let Event::WindowEvent { event, .. } = event {
+            match event {
+                Closed => return ControlFlow::Break,
+                KeyboardInput { input, .. } => {
+                    if let Some(VirtualKeyCode::Q) = input.virtual_keycode {
+                        if cfg!(target_os = "macos") && input.modifiers.logo {
+                            return ControlFlow::Break;
                         }
                     }
-                    CursorMoved {
-                        position: (x, y), ..
-                    } => mouse_state.pos = (x as i32, y as i32),
-                    MouseInput { state, button, .. } => match button {
-                        MouseButton::Left => mouse_state.pressed.0 = state == Pressed,
-                        MouseButton::Right => mouse_state.pressed.1 = state == Pressed,
-                        MouseButton::Middle => mouse_state.pressed.2 = state == Pressed,
-                        _ => {}
-                    },
-                    MouseWheel {
-                        delta: MouseScrollDelta::LineDelta(_, y),
-                        phase: TouchPhase::Moved,
-                        ..
-                    }
-                    | MouseWheel {
-                        delta: MouseScrollDelta::PixelDelta(_, y),
-                        phase: TouchPhase::Moved,
-                        ..
-                    } => mouse_state.wheel = y,
-                    ReceivedCharacter(c) => imgui.add_input_character(c),
-                    _ => (),
                 }
+                CursorMoved {
+                    position: (x, y), ..
+                } => {
+                    mouse_state.pos = (x as i32, y as i32);
+                    update_mouse(&mut imgui, &mut mouse_state);
+
+                    let now = Instant::now();
+                    let delta = now - last_frame;
+                    if delta < FRAME_DURATION {
+                        return ControlFlow::Continue;
+                    }
+                    let delta_s =
+                        delta.as_secs() as f32 + delta.subsec_nanos() as f32 / 1_000_000_000.0;
+                    last_frame = now;
+
+                    if !render(
+                        &mut imgui,
+                        &mut run_ui,
+                        &display,
+                        &mut renderer,
+                        clear_color,
+                        delta_s,
+                    ) {
+                        return ControlFlow::Break;
+                    }
+                }
+                MouseInput { state, button, .. } => if MouseButton::Left == button {
+                    mouse_state.pressed.0 = state == Pressed;
+                    update_mouse(&mut imgui, &mut mouse_state);
+
+                    let now = Instant::now();
+                    let delta = now - last_frame;
+                    let delta_s =
+                        delta.as_secs() as f32 + delta.subsec_nanos() as f32 / 1_000_000_000.0;
+                    last_frame = now;
+
+                    // Render twice to immediately show the results of the click
+                    if !render(
+                        &mut imgui,
+                        &mut run_ui,
+                        &display,
+                        &mut renderer,
+                        clear_color,
+                        delta_s,
+                    ) {
+                        return ControlFlow::Break;
+                    }
+                    if !render(
+                        &mut imgui,
+                        &mut run_ui,
+                        &display,
+                        &mut renderer,
+                        clear_color,
+                        delta_s,
+                    ) {
+                        return ControlFlow::Break;
+                    }
+                },
+                _ => (),
             }
-        });
-
-        if quit {
-            break;
         }
-
-        let now = Instant::now();
-        let delta = now - last_frame;
-        let delta_s = delta.as_secs() as f32 + delta.subsec_nanos() as f32 / 1_000_000_000.0;
-        last_frame = now;
-
-        update_mouse(&mut imgui, &mut mouse_state);
-
-        let gl_window = display.gl_window();
-        let size_pixels = gl_window.get_inner_size().unwrap();
-        let hidpi = gl_window.hidpi_factor();
-        let size_points = (
-            (size_pixels.0 as f32 / hidpi) as u32,
-            (size_pixels.1 as f32 / hidpi) as u32,
-        );
-
-        let ui = imgui.frame(size_points, size_pixels, delta_s);
-        if !run_ui(&ui, (size_points.0 as f32, size_points.1 as f32)) {
-            break;
-        }
-
-        let mut target = display.draw();
-        target.clear_color(
-            clear_color[0],
-            clear_color[1],
-            clear_color[2],
-            clear_color[3],
-        );
-        renderer.render(&mut target, ui).expect("Rendering failed");
-        target.finish().unwrap();
-    }
+        ControlFlow::Continue
+    });
 }
 
-fn configure_keys(imgui: &mut ImGui) {
-    use imgui::ImGuiKey;
+pub fn render<F: FnMut(&Ui, (f32, f32)) -> bool>(
+    imgui: &mut ImGui,
+    mut run_ui: F,
+    display: &Display,
+    renderer: &mut Renderer,
+    clear_color: [f32; 4],
+    delta_s: f32,
+) -> bool {
+    let gl_window = display.gl_window();
+    let size_pixels = gl_window.get_inner_size().unwrap();
+    let hidpi = gl_window.hidpi_factor();
+    let size_points = (
+        (size_pixels.0 as f32 / hidpi) as u32,
+        (size_pixels.1 as f32 / hidpi) as u32,
+    );
 
-    imgui.set_imgui_key(ImGuiKey::Tab, 0);
-    imgui.set_imgui_key(ImGuiKey::LeftArrow, 1);
-    imgui.set_imgui_key(ImGuiKey::RightArrow, 2);
-    imgui.set_imgui_key(ImGuiKey::UpArrow, 3);
-    imgui.set_imgui_key(ImGuiKey::DownArrow, 4);
-    imgui.set_imgui_key(ImGuiKey::PageUp, 5);
-    imgui.set_imgui_key(ImGuiKey::PageDown, 6);
-    imgui.set_imgui_key(ImGuiKey::Home, 7);
-    imgui.set_imgui_key(ImGuiKey::End, 8);
-    imgui.set_imgui_key(ImGuiKey::Delete, 9);
-    imgui.set_imgui_key(ImGuiKey::Backspace, 10);
-    imgui.set_imgui_key(ImGuiKey::Enter, 11);
-    imgui.set_imgui_key(ImGuiKey::Escape, 12);
-    imgui.set_imgui_key(ImGuiKey::A, 13);
-    imgui.set_imgui_key(ImGuiKey::C, 14);
-    imgui.set_imgui_key(ImGuiKey::V, 15);
-    imgui.set_imgui_key(ImGuiKey::X, 16);
-    imgui.set_imgui_key(ImGuiKey::Y, 17);
-    imgui.set_imgui_key(ImGuiKey::Z, 18);
+    let ui = imgui.frame(size_points, size_pixels, delta_s);
+    if !run_ui(&ui, (size_points.0 as f32, size_points.1 as f32)) {
+        return false;
+    }
+
+    let mut target = display.draw();
+    target.clear_color(
+        clear_color[0],
+        clear_color[1],
+        clear_color[2],
+        clear_color[3],
+    );
+    renderer.render(&mut target, ui).expect("Rendering failed");
+    target.finish().unwrap();
+    true
 }
 
 fn update_mouse(imgui: &mut ImGui, mouse_state: &mut MouseState) {
